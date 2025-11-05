7e29693a4693d1fbb33f799447dc65d2e71713dc
liam <1371483737@qq.com>
2025-11-05 17:14:18 +0800

    remove some files

--- build/auto_aim_interfaces/auto_aim_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ljoy/Robomaster/PIONEER/new_rv/build/auto_aim_interfaces/auto_aim_interfaces__py
-# Build directory: /home/ljoy/Robomaster/PIONEER/new_rv/build/auto_aim_interfaces/auto_aim_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rm_vision_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ljoy/Robomaster/PIONEER/new_rv/install/auto_aim_interfaces/share/auto_aim_interfaces/package.sh"
-. "/home/ljoy/Robomaster/PIONEER/new_rv/install/rm_serial_driver/share/rm_serial_driver/package.sh"
