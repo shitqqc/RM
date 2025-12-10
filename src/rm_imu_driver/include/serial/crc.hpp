0ad43b2f3af0a7f7bee5d47e4b85f6d5e4e16df4
tomoon <2085382282@qq.com>
2025-12-10 22:04:08 +0800

    feature:imu_driver

---
+++ src/rm_imu_driver/include/serial/crc.hpp
@@ -0,0 +1,16 @@
+#ifndef TOOLS__CRC_HPP
+#define TOOLS__CRC_HPP
+
+#include <cstdint>
+
+namespace tools
+{
+// len不包括crc16
+uint16_t get_crc16(const uint8_t * data, uint32_t len);
+
+// len包括crc16
+bool check_crc16(const uint8_t * data, uint32_t len);
+
+}  // namespace tools
+
+#endif  // TOOLS__CRC_HPP
