0a7fe5e83bb5002d0f52a9abcd4edda24d92b22e
Sam <aa@qq.com>
2026-01-29 15:55:54 +0800

    omnitest

--- src/rm_vision/rm_vision_bringup/launch/vision_bringup.launch.py
+++ src/rm_vision/rm_vision_bringup/launch/vision_bringup.launch.py
@@ -7,57 +7,17 @@ sys.path.append(os.path.join(get_package_share_directory('rm_vision_bringup'), '
 def generate_launch_description():
 
     from common import node_params, launch_params, robot_state_publisher, armor_tracker_node
-    from launch_ros.descriptions import ComposableNode
-    from launch_ros.actions import ComposableNodeContainer, Node
-    from launch.actions import TimerAction, Shutdown
+    from launch_ros.actions import Node
+    from launch.actions import TimerAction, Shutdown, IncludeLaunchDescription
+    from launch.launch_description_sources import PythonLaunchDescriptionSource
     from launch import LaunchDescription
 
-    def get_camera_node(package, plugin):
-        return ComposableNode(
-            package=package,
-            plugin=plugin,
-            name='camera_node',
-            parameters=[node_params],
-            extra_arguments=[{'use_intra_process_comms': True}]
+    camera_sync_dir = get_package_share_directory('camera_sync')
+    multi_camera_vision = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(camera_sync_dir, 'launch', 'multi_camera_vision.launch.py')
         )
-
-    def get_camera_detector_container(camera_node):
-        return ComposableNodeContainer(
-            name='camera_detector_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                camera_node,
-                ComposableNode(
-                    package='armor_detector',
-                    plugin='rm_auto_aim::ArmorDetectorNode',
-                    name='armor_detector',
-                    parameters=[node_params],
-                    extra_arguments=[{'use_intra_process_comms': True}]
-                ),
-                # ComposableNode(
-                #     package='buff_detector',
-                #     plugin='rm_buff::BuffDetectorNode',
-                #     name='buff_detector',
-                #     parameters=[node_params],
-                #     extra_arguments=[{'use_intra_process_comms': True}]
-                # )
-            ],
-            output='both',
-            emulate_tty=True,
-            ros_arguments=['--ros-args', '--log-level',
-                           'armor_detector:='+launch_params['armor_detector_log_level']],
-            on_exit=Shutdown(),
-        )
-
-    hik_camera_node = get_camera_node('hik_camera', 'hik_camera::HikCameraNode')
-    mv_camera_node = get_camera_node('mindvision_camera', 'mindvision_camera::MVCameraNode')
-
-    if (launch_params['camera'] == 'hik'):
-        cam_detector = get_camera_detector_container(hik_camera_node)
-    elif (launch_params['camera'] == 'mv'):
-        cam_detector = get_camera_detector_container(mv_camera_node)
+    )
 
     # 串口
     if launch_params['virtual_serial']:
@@ -119,7 +79,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         robot_state_publisher,
-        cam_detector,       
+        multi_camera_vision,
         delay_serial_node,
         delay_armor_tracker_node,
         delay_planner_node
